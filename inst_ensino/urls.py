252ec52bb908fb21147ac873f64adba72235f8c9
Denilson Fagundes Barbosa <denilsonfbar@gmail.com>
2022-02-03 16:43:41 -0300

    Created main authentication

--- inst_ensino/urls.py
+++ inst_ensino/urls.py
@@ -23,3 +23,8 @@ urlpatterns = [
 urlpatterns += [
     path('', include('academico.urls')),
 ]
+
+#Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
